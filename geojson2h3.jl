5a4043cfc6b7312d5cf61a46e8f59c68b077764b
Oliver Blanthorn <freedom4cows@gmail.com>
2024-11-27 13:51:27 +0100

    Totally replace code

--- geojson2h3.jl
+++ geojson2h3.jl
@@ -1,33 +1,55 @@
-using LazyJSON, DataFrames, ThreadsX
-geojson = LazyJSON.value(read("data/Lower_layer_Super_Output_Areas_2021_EW_BGC_V3_-6823567593069184824.geojson", String));
-# consider using LazyJSON.value(String(Mmap.mmap(open("filename","r"))))
-H3_RES = 11
+#!/bin/julia
 
-geojson["features"][1]["properties"]
+# usage: ./geojson2h3.jl input.geojson output.arrow
 
-# edit this for each geojson
-featmeta(feat) = begin
-    CODE_KEY = "LSOA21CD"
-    NAME_KEY = "LSOA21NM"
-    (name=feat["properties"][NAME_KEY], code=feat["properties"][CODE_KEY],)
+using JSON, DataFrames, Statistics, Arrow
+
+infile = ARGS[1]
+outfile = ARGS[2]
+
+# readchomp(`h3 polygonToCells --help`) |> print
+
+geo = JSON.parsefile(infile)
+features = geo["features"]
+
+function featureToH3(feature)
+    buffer = IOBuffer()
+    geoms = feature["geometry"]["coordinates"]
+    geoms_rev = map(x -> reverse.(x), geoms) # easter egg: opposite to geojson
+    open(`h3 polygonToCells -r 15 -i --`, "w", buffer) do io
+        println(io, geoms_rev) # easter egg: polygonToCells will take a geojson fragment from Julia, but it's slower
+    end
+    return take!(buffer) |> String |> JSON.parse
 end
 
-hexdf = reduce(vcat, ThreadsX.map(feat -> begin
-    pipe_in = IOBuffer(string(feat))
-    hexstrarray = try
-        read(pipeline(addenv(`bun run geojson2h3.js`, "DENO_H3_RES" => H3_RES), stdin=pipe_in), String)
-    catch(e)
+function bbox(coordinates)
+    # coordinats are pairs of [[x, y], [x, y], ...]
+    minx = minimum(map(x -> x[1], coordinates))
+    maxx = maximum(map(x -> x[1], coordinates))
+    miny = minimum(map(x -> x[2], coordinates))
+    maxy = maximum(map(x -> x[2], coordinates))
+    return [minx, miny, maxx, maxy]
+end
+
+function bboxarea(bbox)
+    (bbox[3] - bbox[1]) * (bbox[4] - bbox[2])
+end
+
+
+h3s = Dict{Int, Vector{Any}}()
+area_cutoff = quantile(map(f -> (bboxarea∘bbox)(f["geometry"]["coordinates"][1]), features), 0.99)
+length_cutoff = quantile(map(f -> length(f["geometry"]["coordinates"][1]), features),0.99)
+for feature in features # multithreading doesn't work :(
+    (any(map(>(area_cutoff)∘bboxarea∘bbox, feature["geometry"]["coordinates"])) || any(map(>(length_cutoff)∘length, feature["geometry"]["coordinates"]))) && continue # skip big polygons
+    try
+        h3s[feature["properties"]["INSPIREID"]] = featureToH3(feature)
+    catch (e)
         @warn e
-        ["0x0"]
     end
-    map(x -> (featmeta(feat)..., hex=x,), parse.(UInt64, LazyJSON.value(hexstrarray), base=16))
-end, geojson["features"])) |> DataFrame
+end
 
-using CSV, Statistics
-CSV.write("lsoa_7.csv", hexdf)
+df = flatten(DataFrame(:id_str => keys(h3s)|>collect, :h3_str => values(h3s)|>collect), :h3_str)
+df.h3 = map(x -> parse(UInt64, x, base=16), df.h3_str)
+df.INSPIREID = UInt32.(df.id_str)
 
-df = leftjoin(hexdf, combine(groupby(hexdf, :code), x -> (value_t = rand(),)), on=:code)
-df.index = string.(df.hex, base=16)
-df.value = 1 .- (df.value_t .- quantile(df.value_t, 0.05)) ./ quantile(df.value_t, 0.95) # 0 is bad (red), cut off top and bottom 5%
-CSV.write("$(homedir())/projects/H3-MON/www/data/h3_data.csv", df[!, [:index, :value]])
-
+Arrow.write(outfile, df[!, [:INSPIREID, :h3]]) # 73MB for IOW with big polygons excluded, approx 5mins to run
--- readme.md
+++ readme.md
@@ -1,5 +1,27 @@
-# Convert GeoJSON to DataFrame of H3 UInts
+# rough script for converting geojson files to arrow files of h3 cells using the h3 CLI
 
-Needs Bun. Run with `julia --project=. --threads auto`. 
+requires h3 on the PATH
+```
+yay -S h3-git
+h3 polygonToCells --help
+./geojson2h3.jl input.geojson output.arrow
+```
 
-Rough script, edit it for your own needs.
+
+# example: inspire polygons
+
+takes a while
+
+```
+mkdir out/
+ls ~/projects/inspire/data/*.geojson | parallel -j12 --bar --eta 'julia --project=. geojson2h3.jl {} out/{/.}.arrow'
+# {//} is the directory name, {/.} is the file name without extension, {} is the whole path
+```
+
+check for entirely missing ones
+
+```fish
+ls (comm -3 (ls ~/projects/inspire/data/*.geojson | xargs basename -s .geojson | sort | psub) (ls out/*.arrow | xargs basename -s .arrow | sort | psub) | sed 's,.*,/home/oliver/projects/inspire/data/&.geojson,') | parallel -j12 --bar --eta 'julia --project=. geojson2h3.jl {} out/{/.}.arrow'
+``` 
+
+todo: log failed IDs
